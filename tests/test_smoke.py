844612de3c91a17b6978d7beda254b976cdd9097
Vitaliy Zakharov <zak125@mail.ru>
2024-08-07 17:32:20 +0300

    added smoke tests

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -7,3 +7,14 @@ def test_app_status(app_url):
     response = requests.get(f"{app_url}/status/")
     assert response.status_code == HTTPStatus.OK
     assert response.json()["database"], "Init data are absent"
+
+
+def test_service_response_time(app_url):
+    response = requests.get(app_url)
+    assert response.elapsed.total_seconds() < 1
+
+
+def test_service_headers(app_url):
+    response = requests.get(app_url)
+    assert 'Content-Type' in response.headers
+    assert response.headers['Content-Type'] == 'application/json'
